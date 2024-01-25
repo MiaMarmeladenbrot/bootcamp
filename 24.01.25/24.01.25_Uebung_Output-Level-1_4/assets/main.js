cf8e36c45ff8b3b24fae3ed896dfe3b978fd776c
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-01-25 14:41:04 +0100

    output uebung

---
+++ 24.01.25/24.01.25_Uebung_Output-Level-1_4/assets/main.js
@@ -0,0 +1,22 @@
+// - Lernziel: Verschiedene Möglichkeiten (alert, prompt, confirm) für den Output.
+// - Nun beschäftigst du dich mit dem Thema Output. Du kannst dir im JS auf unterschiedliche Art und Weise Daten anzeigen lassen.
+// - Teste dazu folgenden Code und die nachstehenden Befehle. Gehe Befehl für Befehl durch und kommentiere dann die jeweilige(n) Zeile(n) aus, sonst liegen die ganzen Pop-Up-Fenster übereinander:
+
+// window.alert("Hallo Welt");
+
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+
+// window.confirm("Stimmen Sie meiner Meinung zu?");
+
+// let alter = window.prompt("Gib mir bitte dein Alter:");
+// console.log(alter);
+
+// let b = 5;
+// let a = b * 5 - 3;
+// window.alert(a);
+
+// let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+// let grosse = "2,72m";
+// window.confirm(mann + grosse);
