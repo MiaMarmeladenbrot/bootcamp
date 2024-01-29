2f9d5c3487f62cecc516c62bc0988e618a74f31b
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-01-29 16:03:30 +0100

    Tobis Lösung ergänzt

--- 24.01.29/24.01.29_Uebung_Forms-JS-Level-3_3/assets/js/main.js
+++ 24.01.29/24.01.29_Uebung_Forms-JS-Level-3_3/assets/js/main.js
@@ -14,3 +14,21 @@ function changeBackground() {
   wrapper.style.backgroundColor =
     "rgb(" + bgRot + "," + bgGruen + "," + bgBlau + ")";
 }
+
+// * Lösung von Tobi:
+// function changeBackground() {
+//   const red = document.querySelector("#red").value;
+//   const green = document.querySelector("#green").value;
+//   const blue = document.querySelector("#blue").value;
+
+//   console.log(red, green, blue);
+
+//   const farbe = `rgb(${red}, ${green}, ${blue})`
+//   console.log(farbe);
+
+//   //rbg(0, 0, 0);
+//   document.querySelector("body").style.backgroundColor = farbe;
+//   document.querySelector("body").style.color = "#FFFFFF";
+// }
+
+// changeBackground();
