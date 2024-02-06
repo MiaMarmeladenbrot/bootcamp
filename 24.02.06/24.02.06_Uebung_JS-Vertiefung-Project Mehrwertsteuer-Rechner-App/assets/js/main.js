a9c922017d6a02d36aaec3c745d80c10cace1b59
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-02-06 15:36:53 +0100

    06.02.2024 Uebung MWS Rechner

--- 24.02.06/24.02.06_Uebung_JS-Vertiefung-Project Mehrwertsteuer-Rechner-App/assets/js/main.js
+++ 24.02.06/24.02.06_Uebung_JS-Vertiefung-Project Mehrwertsteuer-Rechner-App/assets/js/main.js
@@ -9,24 +9,32 @@
 // 3. Formular stylen ✅
 // 4. values auslesen ✅
 // 4.1. wenn substract ausgewählt ist, Bruttobetrags-Input anzeigen --> neue Funktion mit radio-Button verknüpfen? ✅
-// 5. outputs festlegen (außerhalb der Funktion)
-// 6. if/else?
+// 5. outputs festlegen (außerhalb der Funktion)✅
+// 6. if/else für die vier Fälle NzB+19, NzB+7, BzN+19, BzN+7✅
 
-// ! Funktion, um bei Auswahl des ersten Radio-Buttons das Eingabe-Textfeld zu ändern (verknüpft via onchange mit beiden radio-Buttons):
+// ! Funktion, um bei Auswahl des ersten Radio-Buttons "Was soll berechnet werden?" das Eingabe-Textfeld zu ändern (verknüpft via onchange mit beiden radio-Buttons):
 const changeToSubstract = () => {
+  const addOrSubstract = document.querySelector(
+    "input[name='was']:checked"
+  ).value;
+
   const betragInput = document.querySelector(".betrag-eingabe");
   // console.log(betragInput);
 
-  const substractInput = document.querySelector("#substract").checked;
-  // console.log(substractInput);
+  const bruttoNettoOutput = document.querySelector(".brutto-netto-output");
+  // console.log(bruttoNettoOutput);
 
-  const addInput = document.querySelector("#add").checked;
-  // console.log(addInput);
-
-  if (substractInput === true) {
+  // wenn MWS abziehen ausgewählt, soll Eingabefeld zu Brutto und Ausgabefeld zu Netto geändert werden:
+  if (addOrSubstract === "substract") {
     betragInput.innerHTML = `<label for="betrag7">Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro<input type="number" id="betrag7" value="1" min="1" name="betrag"/></label>`;
-  } else if (addInput === true) {
+
+    bruttoNettoOutput.innerHTML = `<h2 class="brutto-netto-output">Nettobetrag</h2>`;
+
+    // wenn MWS aufschlagen ausgewählt, soll Eingabefeld zu Netto und Ausgabefeld zu Brutto geändert werden:
+  } else if (addOrSubstract === "add") {
     betragInput.innerHTML = `<label for="betrag19">Nettobetrag (Preis ohne Mehrwertsteuer) in Euro<input type="number" id="betrag19" value="1" min="1" name="betrag"/></label>`;
+
+    bruttoNettoOutput.innerHTML = `<h2 class="brutto-netto-output">Bruttobetrag (Endpreis)</h2>`;
   } else {
     betragInput.innerHTML = `<p> Fehler </p>`;
   }
@@ -35,10 +43,14 @@ const changeToSubstract = () => {
 // ! Output-Werte außerhalb der folgenden Funktion, falls man später noch mal damit arbeiten will:
 const outputMws = document.querySelector(".mws");
 const outputBruttobetrag = document.querySelector(".bruttobetrag");
+// console.log(outputMws);
+// console.log(outputBruttobetrag);
 
 // ! Funktion, um bei Klick auf Berechnen die Ergebnisse in den Output-Feldern darunter auszugeben:
 const mwsRechner = (event) => {
   event.preventDefault();
+
+  //* values auslesen:
   // value auslesen für radio-Buttons, was berechnet werden soll:
   const addOrSubstract = document.querySelector(
     "input[name='was']:checked"
@@ -53,7 +65,34 @@ const mwsRechner = (event) => {
   const numberInput = document.querySelector("input[name='betrag']").value;
 
   // logs
-  console.log({ addOrSubstract });
-  console.log({ nineteenOrSeven });
-  console.log({ numberInput });
+  // console.log({ addOrSubstract });
+  // console.log({ nineteenOrSeven });
+  // console.log({ numberInput });
+
+  //* Berechnung mit if/else-Bedingungen:
+  // wenn NzB & 19% => Betrag * 0.19 & Betrag * 1,19:
+  if (addOrSubstract === "add" && nineteenOrSeven === "prozent19") {
+    outputMws.innerHTML = Math.round(numberInput * 0.19);
+    outputBruttobetrag.innerHTML = Math.round(numberInput * 1.19);
+
+    // wenn NzB & 7% => Betrag * 0.07 & Betrag * 1,07:
+  } else if (addOrSubstract === "add" && nineteenOrSeven === "prozent7") {
+    outputMws.innerHTML = Math.round(numberInput * 0.07);
+    outputBruttobetrag.innerHTML = Math.round(numberInput * 1.07);
+
+    // wenn BzN & 19% => Betrag / (1,19 * 0,19) & Betrag / 1,19:
+  } else if (
+    addOrSubstract === "substract" &&
+    nineteenOrSeven === "prozent19"
+  ) {
+    outputMws.innerHTML = Math.round((numberInput / 1.19) * 0.19);
+    outputBruttobetrag.innerHTML = Math.round(numberInput / 1.19);
+
+    // wenn BzN & 7% => Betrag / (1,07 * 0,07) & Betrag / 1,07:
+  } else if (addOrSubstract === "substract" && nineteenOrSeven === "prozent7") {
+    outputMws.innerHTML = Math.round((numberInput / 1.07) * 0.07);
+    outputBruttobetrag.innerHTML = Math.round(numberInput / 1.07);
+  } else {
+    console.log("error");
+  }
 };
