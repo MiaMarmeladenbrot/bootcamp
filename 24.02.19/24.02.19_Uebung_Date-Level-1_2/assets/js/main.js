4e752f27122b60b40cb3ddbf09f1020e96e04c65
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-02-19 16:18:38 +0100

    Korrekturen

--- 24.02.19/24.02.19_Uebung_Date-Level-1_2/assets/js/main.js
+++ 24.02.19/24.02.19_Uebung_Date-Level-1_2/assets/js/main.js
@@ -5,7 +5,8 @@
 // - Du siehst hier das aktuelle Datum des Screenshots! Dein Ergebnis orientiert sich an dem aktuellen Datum.
 // - Erstelle ein schönes CSS-Design für deine elektronische Uhr und füge ein festes Datum ein. Dieses Design werden wir später verwenden, um eine funktionierende Uhr zu bauen!
 
-// ! Ausgaben im HTML
+// ! Ausgaben vom aktuellen Datum im HTML
+// Variablen festlegen:
 const output = document.querySelector(".output");
 const heute = new Date();
 const jahr = heute.getFullYear();
@@ -16,6 +17,7 @@ const minute = heute.getMinutes();
 const wochentag = heute.toLocaleString("default", { weekday: "long" });
 const monatLang = heute.toLocaleString("default", { month: "long" });
 
+// ins HTML schreiben:
 output.innerHTML += `<p>${heute}</p>`;
 output.innerHTML += `<p>${jahr}</p>`;
 output.innerHTML += `<p>${monat} Monat</p>`;
@@ -25,36 +27,51 @@ output.innerHTML += `<p>${minute} Minute</p>`;
 output.innerHTML += `<p>${wochentag}</p>`;
 output.innerHTML += `<p>${monatLang}</p>`;
 
-// ! Uhr
+// ! Uhr mit aktueller Zeit
+// Outputs:
 const outputWeekday = document.querySelector(".weekday");
 const outputHours = document.querySelector(".hours");
 const outputMinutes = document.querySelector(".minutes");
 const outputSeconds = document.querySelector(".seconds");
 const outputPeriod = document.querySelector(".period");
 
+// noch fehlende Variablen:
 const weekdayShort = heute.toLocaleString("default", { weekday: "short" });
 const second = heute.getSeconds();
 
+// ins HTML schreiben:
 outputWeekday.innerHTML = weekdayShort;
 outputHours.innerHTML = stunde;
 outputMinutes.innerHTML = minute;
 outputSeconds.innerHTML = second;
 
-// ! Umwandlung von Uhrzeiten zu AM/PM
+// ! Rumprobieren: Umwandlung von Uhrzeiten zu AM/PM
+const outputAmPm = document.querySelector(".am-pm");
+
+// via locales-Änderung, zB auf US English:
+const heuteUS = heute.toLocaleTimeString("en-US");
+console.log(heuteUS); //--> 3:59:55 PM
+outputAmPm.innerHTML += `<p>${heuteUS}</p>`;
+
+// gleichzeitige Beschränkung auf Stunden und Minuten und beides immer zweistellig:
+const heuteUS2Digits = heute.toLocaleTimeString("en-US", {
+  hour: "2-digit",
+  minute: "2-digit",
+});
+console.log(heuteUS2Digits); //--> 03:59 PM
+outputAmPm.innerHTML += `<p>${heuteUS2Digits}</p>`;
+
 // das heutige Datum und Uhrzeit in AM/PM anzeigen:
 const heuteAmPMDateTime = heute.toLocaleString("default", { hour12: true });
 console.log(heuteAmPMDateTime); //--> 19.2.2024, 3:40:31 PM
+outputAmPm.innerHTML += `<p>${heuteAmPMDateTime}</p>`;
 
-// nur heutige Uhrzeit in AM/PM anzeigen:
+// nur aktuelle Uhrzeit in AM/PM anzeigen:
 const heuteAmPMTime = heute.toLocaleTimeString("default", { hour12: true });
 console.log(heuteAmPMTime); //--> 3:36:17 PM
-
-// beides ins HTML schreiben:
-const outputAmPm = document.querySelector(".am-pm");
-outputAmPm.innerHTML += `<p>${heuteAmPMDateTime}</p>`;
 outputAmPm.innerHTML += `<p>${heuteAmPMTime}</p>`;
 
-// --> aber: getHours() kann man auf die Variable nun nicht mehr anwenden - weil man das Datum in einen string verändert hat?
+// --> aber: getHours() kann man auf die Variable nun nicht mehr anwenden - weil man das Datum in einen string verändert hat und nicht mehr das ursprüngliche date-Format hat
 
 //* Funktion, die in AM/PM Hours und Minutes zweistellig ändert:
 function changeTimeFormat() {
