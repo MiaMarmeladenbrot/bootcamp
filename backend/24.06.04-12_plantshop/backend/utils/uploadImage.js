1094011663c83477172690de8a95ea8d878626bd
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-06-14 15:13:09 +0200

    add cloudinary folder

--- backend/24.06.04-12_plantshop/backend/utils/uploadImage.js
+++ backend/24.06.04-12_plantshop/backend/utils/uploadImage.js
@@ -12,3 +12,23 @@ export const uploadImage = async (byteArrayBuffer) => {
 
   return uploadResult;
 };
+
+// * Variante mit Speicherung in einem bestimmten cloudinary-Ordner
+// import cloudinary from "cloudinary";
+
+// export const uploadImage = async (byteArrayBuffer) => {
+//   const uploadResult = await new Promise((resolve) => {
+//     cloudinary.v2.uploader
+//       .upload_stream(
+//         { folder: "EventPilot/eventImages" },
+//         (error, uploadResult) => {
+//           console.log(error);
+//           return resolve(uploadResult);
+//         }
+//       )
+//       .end(byteArrayBuffer);
+//   });
+//   console.log(uploadResult);
+
+//   return uploadResult;
+// };
